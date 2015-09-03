68cc2a923743b25dcffd77e907f68a3fb692b263
jgiannuzzo <juan.giannuzzo@black-tobacco.com>
2015-09-03 09:28:35 -0300

    updated entrypoint script for ajenti server

--- ajenti/entrypoint.sh
+++ ajenti/entrypoint.sh
@@ -23,6 +23,18 @@ if [[ ! -d $VOLUME_HOME/mysql ]]; then
     echo "=> Done!"  
 else
     echo "=> Using an existing volume of MySQL"
+    echo "=> Updating root and debian-sys-maint passwords"
+    PASSWD="$(grep -m 1 --only-matching --perl-regex "(?<=password \= ).*" /etc/mysql/debian.cnf)"
+
+    /usr/bin/mysqld_safe &
+    sleep 5s
+
+    /usr/bin/mysqladmin -u root password $MYSQL_ADMIN_PASSWORD
+    
+    echo "SET PASSWORD FOR 'debian-sys-maint'@'localhost' = PASSWORD('$PASSWD');" |mysql -u root --password=$MYSQL_ADMIN_PASSWORD
+
+    killall mysqld    
+    sleep 5s
 fi
 
 mkdir /var/www/sites
